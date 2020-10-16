7eaeae497be6bd925bc6a58a341b958d604edfb0
Alexandre Betioli <apbetioli@gmail.com>
2020-10-16 00:05:29 -0300

    Change api endpoint

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const api = axios.create({baseURL: process.env.REACT_APP_API_URL + '/api'})
+const api = axios.create({baseURL: process.env.REACT_APP_API_URL})
 
 api.defaults.headers.post['Content-Type'] = 'application/json';
 
